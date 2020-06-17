e5db8f1b5e89cc346c783c4fcf17c823339867c9
Arun Lal <arunlalkp.kp@gmail.com>
2020-06-17 16:47:31 +0530

    added video scroll

--- assets/js/local.js
+++ assets/js/local.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     console.log('jQuery loaded!')
-    $(".owl-carousel").owlCarousel({
+    $(".sample-owl-carousel").owlCarousel({
       loop: true,
       margin: 10,
       responsiveClass: true,
