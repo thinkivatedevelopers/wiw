93ab534486fc8257fc3c5f046fccdd5cfa6290a3
Arun Lal <arunlalkp.kp@gmail.com>
2020-07-28 09:12:54 +0530

    package page updations, title and meta test

--- src/app/features-page/features-page.component.ts
+++ src/app/features-page/features-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {Title, Meta} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-features-page',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeaturesPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private title: Title,
+    private meta: Meta
+  ) { }
 
   ngOnInit(): void {
+    this.title.setTitle('Features - WIW Learn');
+    this.meta.updateTag({name: 'description', content: 'my super decription'});
+  }
+
+  ngOndestroy(){
+    this.title.setTitle('sdfsdfsdf');
   }
 
 }
