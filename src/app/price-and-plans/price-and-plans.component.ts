cae6967f26d72dcda3df3960fdf1c03f8bdfad4d
RASHID-TP <rashimarakkar@gmail.com>
2020-08-03 17:02:11 +0530

    home page updated

--- src/app/price-and-plans/price-and-plans.component.ts
+++ src/app/price-and-plans/price-and-plans.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'price-and-plans',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PriceAndPlansComponent implements OnInit {
 
-  constructor() { }
+  courseList;
+
+  constructor(
+    private data: DataService ) {}
 
   ngOnInit(): void {
+    this.courseList = this.data.getAllData();
+    console.log(this.data.getAllData());
   }
 
 }
