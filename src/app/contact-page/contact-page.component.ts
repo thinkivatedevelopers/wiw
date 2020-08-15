b91b119213730e3b822f28dcf68a0bada088f742
RASHID-TP <rashimarakkar@gmail.com>
2020-08-15 11:13:08 +0530

    contact form validation done

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SEOService } from '../seo.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-page',
@@ -7,6 +8,14 @@ import { SEOService } from '../seo.service';
   styleUrls: ['./contact-page.component.css']
 })
 export class ContactPageComponent implements OnInit {
+  phoneNumber = "^(\+\d{1,3}[- ]?)?\d{10}$";
+   form=new FormGroup({
+     name:new FormControl('',Validators.required),
+     email:new FormControl('',[Validators.required,Validators.email]),
+     mobile:new FormControl('',[Validators.required,Validators.minLength(10)]),
+     message:new FormControl('',[Validators.required,Validators.minLength(5)]),
+   });
+
 
   constructor(
     private seoService: SEOService
@@ -15,6 +24,10 @@ export class ContactPageComponent implements OnInit {
   ngOnInit(): void {
     this.seoService.updateTitle('Contact us - WIW Learn');
     this.seoService.updateDescription('Wiw Learn is a One-App-One-Solution for Students. It allows students to access learning solutions mapped to their curriculum. Learn from anywhere, anytime while simultaneously allowing parents to track and evaluate the student’s performance.');
+  }
+
+  onSubmit(){
+    alert(JSON.stringify(this.form.value))
   }
 
 }
